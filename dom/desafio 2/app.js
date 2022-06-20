553b17f52a0d347de099e3887c31674785758eb7
Camila-Isabela <camila_isabela_lins@hotmail.com>
2022-06-20 14:56:04 -0300

    🧮 desafio event.target

--- dom/desafio 2/app.js
+++ dom/desafio 2/app.js
@@ -1,6 +1,16 @@
 new Vue({
     el: '#desafio',
     data: {
-        valor: ''
+        valor: '',
+        valor2: '',
+    },
+    methods: {
+        exibirAlerta() {
+            alert('Parte 1 OK!')
+        },
+        armazenar(event) {
+            this.valor = event.target.value
+        },
+        
     }
 })
